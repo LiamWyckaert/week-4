0b62b6d5b409231a6f758d7fc595fa3a19325996
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 10:52:17 +0200

    oef8

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex08.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex08.py
@@ -1,5 +1,17 @@
-# maanden = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober","november", "decemeber"]
-# getallen = [100, 200, 300, 400]
+#oef 8
+import random
 
-# months= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-# numbers = [100, 200, 300, 400]
+#deze functie kiest een element uit een doorgegeven list
+def kies_element(list_elementen): 
+    #kies en geef terug
+    return random.choice(list_elementen)
+
+
+
+
+
+maanden = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober","november", "decemeber"]
+getallen = [100, 200, 300, 400]
+
+print(f"de gekozen maand is {kies_element(maanden)}")
+print(f"het gekozen getal is {kies_element(getallen)}")
