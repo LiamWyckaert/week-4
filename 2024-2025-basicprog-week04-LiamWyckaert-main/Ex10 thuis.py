3accd39dfe33ceeeec2d359efbe7757c83916e57
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 11:18:41 +0200

    oef 12

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex10 thuis.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex10 thuis.py
@@ -1,3 +1,5 @@
+
+#oef 10
+
 # lijst_getallen = [5, 6, 9, 5 , 6]
 
-# list_numbers = [5, 6, 9, 5 , 6]
--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex12.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex12.py
@@ -1,6 +1,17 @@
-# list_getallen1 = [4, 5, 6, 4]
-# list_getallen2 = [89, 78, 4]
+#oef12
+
+#deze funtie geeft trui terug als er gemeenschappelijke elementen zijn
+#false is het andere geval
+def zijn_totaal_verschillend (parlist1:list, parlist2:list): 
+    for element in parlist1:
+        if element in parlist2:
+            #bingo: element zit ook in list 2
+            return False  
+    return True
 
 
-# list_numbers1 = [4, 5, 6, 4]
-# list_numbers2 = [89, 78, 4]
+list_getallen1 = [4, 5, 6, 4]
+list_getallen2 = [89, 78, 4]
+print(f"zijn deze lists totaal verschillend? {zijn_totaal_verschillend(list_getallen1, list_getallen2)}")
+
+
