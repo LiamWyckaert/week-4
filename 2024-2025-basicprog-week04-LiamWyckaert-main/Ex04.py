e6c04596877777006438d11c5ec2ec9c5d79ed4c
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 09:38:43 +0200

    oef4

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex04.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex04.py
@@ -1,2 +1,10 @@
-# colors1 = ["red", "green", "blue"]
-# colors2 = ["black", "white"]
+#oef4
+
+colors1 = ["red", "green", "blue"]
+colors2 = ["black", "white"]
+
+print(colors1 * 4)
+
+#de list colors2 wordt bij de list color1 toegevoegd
+colors1 += colors2
+print(colors1)
