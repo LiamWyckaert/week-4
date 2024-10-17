0c917e00e3dada36d6c6f5a683035dfda64ce5de
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 11:07:39 +0200

    oef 9

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex09.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex09.py
@@ -1,5 +1,12 @@
-# lijst_getallen = [11, 52, 125, -89, 1245]
-# lijst_woorden = ["jan", "feb", "maa", "apr", "mei"]
+# oef 9
 
-# list_numbers = [11, 52, 125, -89, 1245]
-# list_months = ["jan", "feb", "mar", "apr", "may"]
+def max_en_min_uit_list(elementen):
+    return str(max(elementen)) + " " + str(min(elementen))
+
+
+
+
+lijst_getallen = [11, 52, 125, -89, 1245]
+lijst_woorden = ["jan", "feb", "maa", "apr", "mei"]
+print(f"het resultaat voor de lijst getallen is {max_en_min_uit_list(lijst_getallen)}")
+print(f"het resultaat voor de lijst woorden is {max_en_min_uit_list(lijst_woorden)}")
