6a410c19a1dda148ef939177a728fd2fbb067396
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 14:18:53 +0200

    oef17

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex17.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex17.py
@@ -1,8 +1,20 @@
-# getal1 = int(input("Geef het minimum op:> "))
-# getal2 = int(input("Geef het maximum op:> "))
-# print(f"De vijf geselecteerde getallen hiertussen zijn: {kies_5_getallen(getal1, getal2)}")
+# oef 17
+#return waarde is een list van getallen
+import random
 
-# number1 = int(input("Enter the minimum:> "))
-# number2 = int(input("Enter the maximum:> "))
-# print(f"The five selected numbers in between are: {choose_5_numbers(number1, number2)}")
+def kies_5_getallen(min: int, max: int) -> list[int]: 
+    result = [] 
+    for indexx in range (0,5):
+        getal = random.randint(min,max)
+        if getal not in result: 
+            result.append(getal)
+    return result
+
+
+
+
+getal1 = int(input("Geef het minimum op:> "))
+getal2 = int(input("Geef het maximum op:> "))
+print(f"De vijf geselecteerde getallen hiertussen zijn: {kies_5_getallen(getal1, getal2)}")
+
 
