0b94accd5811ceaf9d399995b4febf9cddaaa6c1
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 10:23:08 +0200

    oef7

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex07.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex07.py
@@ -1,3 +1,26 @@
-# collection1 = [12, 45, -9, -15]
-# collection2 = [12.23, 45.1, 9.478, 15.125, -3.14]
-# collection3 = ["Joerie", "Marie", "Henk", "Stijn"]
+# oef 7 
+
+def geef_info_list(naam_list:str, list_elementen:list) -> str: 
+    resultaat = f"verzameling {naam_list}\n"
+    positie = 0
+    for element in list_elementen:  
+        #opmerking: index geeft de positie van het 1ste voorkomen van het element terug
+        #resultaat = resultaat + f" {element} op positie {list_elementen.index(element)}\n" 
+        # oplossing: hou zelf de positie bij (vergeet ni de positie up te daten) 
+        resultaat = resultaat + f" {element} op positie {positie}\n
+        positie += 1 
+    return resultaat
+
+
+
+
+
+
+
+collection1 = [12, 45, -9, -15]
+collection2 = [12.23, 45.1, 9.478, 15.125, -3.14]
+collection3 = ["Joerie", "Marie", "Henk", "Stijn"]
+
+print(geef_info_list("gehele getallen", collection1))
+print(geef_info_list("kommagetallen" ,collection2))
+print(geef_info_list("vrienden " ,collection3))
