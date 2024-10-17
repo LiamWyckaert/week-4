3cc1bd3acb5441d726509511020b303cc688f2fa
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 09:45:02 +0200

    oef5

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex05.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex05.py
@@ -1,3 +1,10 @@
-# weekdagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+#oef 5 
+weekdagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
 
-# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saterday","Sunday"] 
+# doel: een deel van de elementen uit een list halen
+# enkel de werkdagen van de week
+print(weekdagen[0:5]) 
+
+#de weekendagen van de week
+print(weekdagen[6:7])
+
