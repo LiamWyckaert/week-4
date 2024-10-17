398663789a1ed9e4255729b0c1f3d83bd9af02bf
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 11:34:40 +0200

    oef 13

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex13.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex13.py
@@ -1,2 +1,15 @@
-# list1 = [10, 20, 30, 40, 40, 40, 70, 80, 99]
-# list2 = ['a', 'b', 'c', 'd', 'e', 'f']
+# oef 13
+
+def tel_elementen_binnen_interval (elementen:list, min, max) -> int:
+    aantal = 0 
+   #ik overloop alle elementen
+    for element in elementen:
+        if element >= min and element <= max:
+            aantal += 1
+    return aantal
+
+
+list1 = [10, 20, 30, 40, 40, 40, 70, 80, 99]
+list2 = ['a', 'b', 'c', 'd', 'e', 'f']
+print(f"Het aantal elementen dus 40 en 100 is {tel_elementen_binnen_interval(list1,40,100)}")
+print(f"Het aantal elementen tussen a en e is {tel_elementen_binnen_interval(list2, "a","e")}")
--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex14.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex14.py
@@ -1,5 +1,11 @@
-# list1 = [78, 4, 5, 6, 4]
-# list2 = [89, 78, 4]
-# list3 = ['Tamara', 'Delfien', 'Elke', 'Marijn']
-# list4 = ['Natasja', 'Mieke', 'Tamara', 'Elke', 'Carine']
+#oef 14 
+
+def geef_gemeenschappelijke_elementen (listA:list, listB:list) -> list: 
+    pass
+
+
+list1 = [78, 4, 5, 6, 4]
+list2 = [89, 78, 4]
+list3 = ['Tamara', 'Delfien', 'Elke', 'Marijn']
+list4 = ['Natasja', 'Mieke', 'Tamara', 'Elke', 'Carine']
 
