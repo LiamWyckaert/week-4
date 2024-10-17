7a036f60f0052084b4718f8135abd2f5b71747f1
Liam Wyckaert <Liamwyckaert@gmail.com>
2024-10-17 09:59:20 +0200

    oef6

--- 2024-2025-basicprog-week04-LiamWyckaert-main/Ex06.py
+++ 2024-2025-basicprog-week04-LiamWyckaert-main/Ex06.py
@@ -1,3 +1,17 @@
-# woord = input("Geef een woord op:> ")
+#oef 6
+klinkers = [ ] 
+medeklinkers = []
+woord = input("Geef een woord op:> ")
+#spaties eruit gooien
+woord = woord.replace(" "," ") 
 
-# word = input("Enter a word:> ")
+for letter in woord.lower(): 
+   if letter == 'a' or letter == 'i' or letter == 'o' or letter == 'e' or letter == 'u':
+      if not letter in klinkers:     #dubbele eruit halen
+        klinkers.append(letter)
+   else: 
+      if not letter in medeklinkers: #dubbels eruit halen
+        medeklinkers.append(letter)
+
+print(f"De klinkers zijn: {klinkers}")
+print(f"De medeklinkers zijn: {medeklinkers}")
